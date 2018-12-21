1f70974dfbe64ee2cb28ebfa5e3d13e4de18530f
Barasa <emmanuelbarasa5@gmail.com>
2018-12-21 19:11:09 +0300

    [finishes #162813024]adds api endpoint for fetching all questions

--- app/api/v1/questions/questions.py
+++ app/api/v1/questions/questions.py
@@ -1,1 +1,18 @@
 import flask
+from flask import request,jsonify
+from datetime import datetime
+
+app = flask.Flask('__name__')
+app.config['DEBUG'] = True
+
+#add some dumpy questions
+
+questions = [{'Q.':'What are data structures ?','Posted by':'Gilly','time':datetime.now().strftime('%Y-%m-%dT%H:%M:%S')},
+            {'Q.':'What is an algorithm ?','Posted by':'Mercy','time':datetime.now().strftime('%Y-%m-%dT%H:%M:%S')},
+            {'Q.':'Define OOP ?','Posted by':'Gilly','time':datetime.now().strftime('%Y-%m-%dT%H:%M:%S')},]
+
+@app.route('/app/api/v1/questions',methods=['GET'])
+def showQuestions():
+    return jsonify(questions)
+
+app.run()
