c7b9524a07629f4724d74536f2254917c26978e5
Barasa <emmanuelbarasa5@gmail.com>
2018-12-22 13:23:06 +0300

    [finishes #162813038]adds api endpoint to fetch a single question

--- app/api/v1/questions/questions.py
+++ app/api/v1/questions/questions.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request,jsonify
+from flask import request,jsonify,abort,make_response
 
 app = flask.Flask('__name__')
 app.config['DEBUG'] = True
@@ -12,5 +12,15 @@ questions = [{'id':1,
             {'id':3,
             'Define the term Algorithm':'posted by Goodwill'}]
 
-@app.route('app/api/v1/questions/<question_id>',methods=['GET'])
+@app.route('/app/api/v1/questions/<int:question_id>',methods=['GET'])
 def showUser(question_id):
+    question = [question for question in questions if question['id'] == question_id]
+    if len(question) == 0:
+        abort(404)
+    return jsonify(question[0])
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'The question was Not found !'}), 404)
+
+app.run()
