f77803ae194f4ac70a57013b401d72858ed2a982
Barasa <emmanuelbarasa5@gmail.com>
2018-12-22 13:59:09 +0300

    [starts #162824412]adds api endppoint to fetch all users

---
+++ app/api/v1/users/users.py
@@ -0,0 +1,16 @@
+import flask
+from flask import request,jsonify,abort,make_response
+
+app = flask.Flask('__name__')
+app.config['DEBUG'] = True
+
+#add dummy users
+users = [{'id':1,'name':'jackie','email':'jackie@mariba.com'},
+        {'id':2,'name':'Antony','email':'anto@domain.com'},
+        {'id':3,'name':'Ali','email':'ali@mindyourlanguage.com'},]
+
+@app.route('/app/api/v1/users',methods=['GET'])
+def showUsers():
+    return jsonify(users)
+
+app.run
