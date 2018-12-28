c132fd46411aa1e933b0d097b19cd7fb2a10e053
Barasa <emmanuelbarasa5@gmail.com>
2018-12-28 13:04:32 +0300

    adds tests for fetch users

--- app/tests/v1/tests.py
+++ app/tests/v1/tests.py
@@ -7,10 +7,15 @@ from app import app
 class TestApi(unittest.TestCase):
 
     def test_add_user(self):
-        response = self.app.post('app/api/v1/users',
+        response = self.app.post('/app/api/v1/users',
                                 data=json.dumps(dict(name="Barasa",email="barasa@gmail.com")),
                                 content_type='application/json')
         self.assertEqual(response.status_code,201)
+
+    def test_fetch_user(self):
+        response = self.app.get('/app/api/v1/users')
+        response.assertEqual(response.status_code,200)
+
 
 if __name__ == '__main__':
     unittest.main()
