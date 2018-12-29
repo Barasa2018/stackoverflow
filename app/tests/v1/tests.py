cab8a786b3711fb37695c6562223f8d8a6ef2d17
Barasa <emmanuelbarasa5@gmail.com>
2018-12-29 08:09:33 +0300

    adds the fetch a user test

--- app/tests/v1/tests.py
+++ app/tests/v1/tests.py
@@ -12,9 +12,13 @@ class TestApi(unittest.TestCase):
                                 content_type='application/json')
         self.assertEqual(response.status_code,201)
 
-    def test_fetch_user(self):
+    def test_fetch_users(self):
         response = self.app.get('/app/api/v1/users')
         self.assertEqual(response.status_code,200)
+
+    def test_fetch_user(self):
+        response = self.app.get('/app/api/v1/users/<int:id>')
+        self.asssertEqual(response.ststus_code,200)
 
     def test_add_question(self):
         response = self.app.post('/app/api/v1/questions',
