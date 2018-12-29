1c026f9f8bb5be0590aa03c280d5aa6a9a1bc876
Barasa <emmanuelbarasa5@gmail.com>
2018-12-29 07:50:26 +0300

    adds the post question test

--- app/tests/v1/tests.py
+++ app/tests/v1/tests.py
@@ -14,7 +14,13 @@ class TestApi(unittest.TestCase):
 
     def test_fetch_user(self):
         response = self.app.get('/app/api/v1/users')
-        response.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code,200)
+
+    def test_add_question(self):
+        response = self.app.post('/app/api/v1/questions',
+                                data=json.dumps(dict(title='CSS',description='I need help with inline CSS')),
+                                content_type='application/json')
+        self.assertEqual(response.status_code,201)
 
 
 if __name__ == '__main__':
